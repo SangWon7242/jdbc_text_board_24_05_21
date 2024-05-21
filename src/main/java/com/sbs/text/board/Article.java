484076f137a1147627da25c5ea2b4c47a38285fb
SangWon7242 <gotkdqja@gmail.com>
2024-05-21 21:43:09 +0900

    JDBC SELECT 테스트

--- src/main/java/com/sbs/text/board/Article.java
+++ src/main/java/com/sbs/text/board/Article.java
@@ -2,8 +2,18 @@ package com.sbs.text.board;
 
 public class Article {
   int id;
+  String regDate;
+  String updateDate;
   String title;
   String body;
+
+  public Article(int id, String regDate, String updateDate, String title, String body) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
+    this.title = title;
+    this.body = body;
+  }
 
   public Article(int id, String title, String body) {
     this.id = id;
