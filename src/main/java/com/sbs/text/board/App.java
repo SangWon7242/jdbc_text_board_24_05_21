4ccb7ba5c9a7405d2ca91f2c3fdade188df3a510
SangWon7242 <gotkdqja@gmail.com>
2024-05-23 19:32:33 +0900

    JDBC UPDATE 테스트, Rq 도입, 게시물 수정 기능 구현

--- src/main/java/com/sbs/text/board/App.java
+++ src/main/java/com/sbs/text/board/App.java
@@ -17,7 +17,10 @@ public class App {
     while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
-      if(cmd.equals("/usr/article/write")) {
+
+      Rq rq = new Rq(cmd);
+
+      if(rq.getUrlPath().equals("/usr/article/write")) {
         System.out.println("== 게시물 추가 ==");
         System.out.print("제목) ");
         String title = sc.nextLine();
@@ -88,7 +91,7 @@ public class App {
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
-      else if(cmd.equals("/usr/article/list")) {
+      else if(rq.getUrlPath().equals("/usr/article/list")) {
 
         // 데이터베이스 URL, 사용자 이름, 비밀번호를 설정합니다.
         String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull"; // 데이터베이스 URL
@@ -175,7 +178,84 @@ public class App {
         }
 
       }
-      else if(cmd.equals("exit")) {
+      else if(rq.getUrlPath().equals("/usr/article/modify")) {
+        System.out.println("== 게시물 수정 ==");
+
+        int id = rq.getIntParam("id", 0);
+
+        if(id == 0) {
+          System.out.printf("id를 올바르게 입력해주세요.");
+          return;
+        }
+
+        System.out.print("새 제목 : ");
+        String title = sc.nextLine();
+
+        System.out.print("새 내용 : ");
+        String body = sc.nextLine();
+
+        // 데이터베이스 URL, 사용자 이름, 비밀번호를 설정합니다.
+        String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull"; // 데이터베이스 URL
+        String user = "sbsst"; // 데이터베이스 사용자 이름
+        String password = "sbs123414"; // 데이터베이스 비밀번호
+
+        // Connection 객체 선언
+        Connection conn = null;
+        PreparedStatement prst = null;
+
+        try {
+          // 드라이버 로드 (필요에 따라 생략 가능, 최신 드라이버는 자동 로드)
+          Class.forName("com.mysql.cj.jdbc.Driver");
+
+          // 데이터베이스 연결
+          conn = DriverManager.getConnection(url, user, password);
+
+          // SQL 업데이트
+          String sql = "UPDATE article";
+          sql += " SET title = '%s'".formatted(title);
+          sql += ", `body` = '%s'".formatted(body);
+          sql += " WHERE id = %d;".formatted(id);
+
+          System.out.println("sql : " + sql);
+          prst = conn.prepareStatement(sql);
+
+          // 명령 실행 및 결과 집합 가져오기
+          prst.executeUpdate();
+
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+          System.out.println("드라이버 로딩 실패!!");
+        } catch (SQLException e) {
+          System.out.println("에러 : " + e);
+          System.out.println("연결 실패!!");
+        } finally {
+          // 리소스 해제
+          if (prst != null) {
+            try {
+              prst.close();
+            } catch (SQLException e) {
+              e.printStackTrace();
+            }
+          }
+          // 연결 닫기
+          if (conn != null) {
+            try {
+              conn.close();
+              System.out.println("Connection closed.");
+            } catch (SQLException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+
+        System.out.printf("%d번 게시물이 수정되었습니다.\n", id);
+
+      }
+      else if(rq.getUrlPath().equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+      else if(rq.getUrlPath().equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
       }
---
+++ src/main/java/com/sbs/text/board/Rq.java
@@ -0,0 +1,46 @@
+package com.sbs.text.board;
+
+import com.sbs.text.board.util.Util;
+
+import java.util.Map;
+
+public class Rq {
+  public String url;
+  public Map<String, String> params;
+  public String urlPath;
+
+  public Rq(String url) {
+    this.url = url;
+    params = Util.getParamsFromUrl(this.url);
+    urlPath = Util.getUrlPathFromUrl(this.url);
+  }
+
+  public Map<String, String> getParams() {
+    return params;
+  }
+
+  public String getUrlPath() {
+    return urlPath;
+  }
+
+  public int getIntParam(String paramName, int defaultValue) {
+    if (params.containsKey(paramName) == false) {
+      return defaultValue;
+    }
+
+    try {
+      return Integer.parseInt(params.get(paramName));
+    } catch (NumberFormatException e) {
+      return defaultValue;
+    }
+
+  }
+
+  public String getParam(String paramName, String defaultValue) {
+    if(params.containsKey(paramName) == false) {
+      return defaultValue;
+    }
+
+    return params.get(paramName);
+  }
+}
